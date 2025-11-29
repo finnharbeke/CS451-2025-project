0685efc52bce2c48109dc189cddd290bae6b150d
finnharbeke <code@finnharbeke.ch>
2025-11-29 10:48:23 +0100

    increased logbufsize

--- perfect/src/include/global.h
+++ perfect/src/include/global.h
@@ -11,7 +11,7 @@
 const unsigned char OO = 0;
 const bool STATS = 1;
 
-const unsigned long LOGBUFSIZE = 1 << 8; // 1/4 MiB, so for 128 proc, 32 MiB
+const unsigned long LOGBUFSIZE = 1 << 18; // 1/4 MiB, so for 128 proc, 32 MiB
 const unsigned long LOGLINESIZE = 64; // longest possible log line
 
 const unsigned long MAX_PENDING = 1024; // messages in queue at a time
